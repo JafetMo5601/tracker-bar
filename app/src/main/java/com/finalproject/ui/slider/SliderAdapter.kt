07a5cc18809961a1d72808bd69f5eb4865f9ff18
JafetMo5601 <jafet.moraugalde15@gmail.com>
2022-03-04 14:59:54 -0600

    Create the horizontal menu slider.

---
+++ app/src/main/java/com/finalproject/ui/slider/SliderAdapter.kt
@@ -0,0 +1,74 @@
+package com.finalproject.ui.slider
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
+import com.finalproject.R
+import com.makeramen.roundedimageview.RoundedImageView
+
+class SliderAdapter internal constructor(
+    sliderItems: MutableList<SliderItem>,
+    titlesList: MutableList<String>,
+    viewPager2: ViewPager2
+): RecyclerView.Adapter<SliderAdapter.SliderViewHolder>() {
+
+    private val sliderItems: List<SliderItem>
+    private val viewPager2: ViewPager2
+    private val titlesList: List<String>
+
+    init {
+        this.sliderItems = sliderItems
+        this.titlesList = titlesList
+        this.viewPager2 = viewPager2
+    }
+
+    class SliderViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val imageView: RoundedImageView = itemView.findViewById(R.id.imageSlide)
+        val itemTitle: TextView = itemView.findViewById(R.id.tv_title_slider)
+
+        init {
+            imageView.setOnClickListener{
+                    v: View ->
+                val position = adapterPosition
+                Toast.makeText(itemView.context, "You clicked on item #${position + 1}", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        fun image(sliderItem: SliderItem) {
+            imageView.setImageResource(sliderItem.image)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SliderViewHolder {
+        return SliderViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.slider_item_container,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: SliderViewHolder, position: Int) {
+        holder.image(sliderItems[position])
+        holder.itemTitle.text = titlesList[position]
+
+        if (position == sliderItems.size - 2) {
+            viewPager2.post(runnable)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return sliderItems.size
+    }
+
+    private val runnable = Runnable {
+        sliderItems.addAll(sliderItems)
+        titlesList.addAll(titlesList)
+        notifyDataSetChanged()
+    }
+}
