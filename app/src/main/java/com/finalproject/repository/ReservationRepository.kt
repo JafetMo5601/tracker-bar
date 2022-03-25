fd696aa25bac52890b12d8d7eaa89e2c945adc3e
JafetMo5601 <jafet.moraugalde15@gmail.com>
2022-03-25 17:05:29 -0600

    Create repository for reservation

--- app/src/main/java/com/finalproject/repository/ReservationRepository.kt
+++ app/src/main/java/com/finalproject/repository/ReservationRepository.kt
@@ -1,4 +1,21 @@
 package com.finalproject.repository
 
-class ReservationRepository {
+import androidx.lifecycle.LiveData
+import com.finalproject.data.ReservationDAO
+import com.finalproject.model.Reservation
+
+class ReservationRepository(private val reservationDAO: ReservationDAO) {
+    val getAllData: LiveData<List<Reservation>> = reservationDAO.getAllData()
+
+    suspend fun addReservation(reservation: Reservation) {
+        reservationDAO.addReservation(reservation)
+    }
+
+    suspend fun updateReservation(reservation: Reservation) {
+        reservationDAO.updateReservation(reservation)
+    }
+
+    suspend fun deleteReservation(reservation: Reservation) {
+        reservationDAO.deleteReservation(reservation)
+    }
 }
