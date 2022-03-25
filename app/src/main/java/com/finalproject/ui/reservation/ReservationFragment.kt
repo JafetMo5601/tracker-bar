63b09b726dfb2c4b325d5f6900dc97bd8752b4b6
JafetMo5601 <jafet.moraugalde15@gmail.com>
2022-03-25 17:06:20 -0600

    Create the fragmet that will implement the list of the reservations

--- app/src/main/java/com/finalproject/ui/reservation/ReservationFragment.kt
+++ app/src/main/java/com/finalproject/ui/reservation/ReservationFragment.kt
@@ -1,59 +1,46 @@
-package com.finalproject
+package com.finalproject.ui.reservation
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.finalproject.R
+import com.finalproject.adapter.ReservationAdapter
+import com.finalproject.databinding.FragmentReservationBinding
+import com.finalproject.model.Reservation
+import com.finalproject.viewmodels.ReservationViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ReservationFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ReservationFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var reservationViewModel: ReservationViewModel
+    private var _binding: FragmentReservationBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reservation, container, false)
+        reservationViewModel =
+            ViewModelProvider(this)[ReservationViewModel::class.java]
+        _binding = FragmentReservationBinding.inflate(inflater, container, false)
+
+        val reservationAdapter = ReservationAdapter()
+        val recycler = binding.recycler
+        recycler.adapter = reservationAdapter
+        recycler.layoutManager = LinearLayoutManager(requireContext())
+
+        reservationViewModel = ViewModelProvider(this)[ReservationViewModel::class.java]
+        reservationViewModel.getAllData.observe(viewLifecycleOwner) {
+            reservations -> reservationAdapter.setData(reservations)
+        }
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ReservationFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ReservationFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
