efc3d42236562a99ece8894ee53936813f3043bc
JafetMo5601 <jafet.moraugalde15@gmail.com>
2022-03-25 17:05:42 -0600

    Create DB for project.

--- app/src/main/java/com/finalproject/data/TrackerBarDB.kt
+++ app/src/main/java/com/finalproject/data/TrackerBarDB.kt
@@ -1,4 +1,33 @@
 package com.finalproject.data
 
-class TrackerBarDB {
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.finalproject.model.Reservation
+
+@Database(entities = [Reservation::class], version = 1, exportSchema = false)
+abstract class TrackerBarDB: RoomDatabase() {
+    abstract fun reservationDAO(): ReservationDAO
+
+    companion object {
+        @Volatile
+        private var INSTANCE: TrackerBarDB? = null
+
+        fun getDatabase(context: android.content.Context): TrackerBarDB {
+            var tempInstance = INSTANCE
+
+            if (tempInstance != null) {
+                return tempInstance
+            }
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    TrackerBarDB::class.java,
+                    "tracker_bar_database"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
 }
