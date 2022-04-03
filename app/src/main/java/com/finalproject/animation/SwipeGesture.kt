ba9c46f40aa43a6edf6a1f013fcc328e421765ee
JafetMo5601 <jafet.moraugalde15@gmail.com>
2022-04-02 23:18:58 -0600

    Add functionality of the swipe gesture to the adapter

--- app/src/main/java/com/finalproject/animation/SwipeGesture.kt
+++ app/src/main/java/com/finalproject/animation/SwipeGesture.kt
@@ -1,19 +1,37 @@
 package com.finalproject.animation
 
+import android.content.Context
+import android.graphics.Canvas
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
+//import it.xabaras.android.recyclerview.swipedecorator.RecyclerViewSwipeDecorator
 
-abstract class SwipeGesture : ItemTouchHelper.SimpleCallback(
-    0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+
+abstract class SwipeGesture(context: Context) : ItemTouchHelper.SimpleCallback(
+    0, ItemTouchHelper.LEFT) {
     override fun onMove(
         recyclerView: RecyclerView,
         viewHolder: RecyclerView.ViewHolder,
         target: RecyclerView.ViewHolder,
     ): Boolean {
-        TODO("Not yet implemented")
+        return false
     }
 
-    override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
-        TODO("Not yet implemented")
+    override fun onChildDraw(
+        c: Canvas,
+        recyclerView: RecyclerView,
+        viewHolder: RecyclerView.ViewHolder,
+        dX: Float,
+        dY: Float,
+        actionState: Int,
+        isCurrentlyActive: Boolean,
+    ) {
+//        RecyclerViewSwipeDecorator.Builder(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+//            .addBackgroundColor(ContextCompat.getColor(this@MainActivity, R.color.my_background))
+//            .addActionIcon(R.drawable.my_icon)
+//            .create()
+//            .decorate()
+
+        super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
     }
 }
