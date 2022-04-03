b4233d366403c060ec49ffd48389a8638231ebca
JafetMo5601 <jafet.moraugalde15@gmail.com>
2022-04-02 23:14:45 -0600

    Create layout files for the reservation page and thge respective icons needed.

---
+++ app/src/main/java/com/finalproject/ReservationPage.kt
@@ -0,0 +1,11 @@
+package com.finalproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ReservationPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reservation_page)
+    }
+}
