676b5f35607bb2bac872ded6bcb5e43716a8acac
JafetMo5601 <jafet.moraugalde15@gmail.com>
2022-04-02 23:19:23 -0600

    Add hardcoded data to show a list of reservations

--- app/src/main/java/com/finalproject/ui/reservation/ReservationFragment.kt
+++ app/src/main/java/com/finalproject/ui/reservation/ReservationFragment.kt
@@ -1,13 +1,12 @@
 package com.finalproject.ui.reservation
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.finalproject.R
 import com.finalproject.adapter.ReservationAdapter
 import com.finalproject.databinding.FragmentReservationBinding
 import com.finalproject.model.Reservation
@@ -28,12 +27,30 @@ class ReservationFragment : Fragment() {
 
         val reservationAdapter = ReservationAdapter()
         val recycler = binding.recycler
+//        val swipeGesture = object: SwipeGesture() {
+//            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+//                when(direction){
+//                    ItemTouchHelper.LEFT -> {
+//                        reservationAdapter.deleteItem(viewHolder.adapterPosition)
+//                    }
+//                }
+//                super.onSwiped(viewHolder, direction)
+//            }
+//        }
         recycler.adapter = reservationAdapter
         recycler.layoutManager = LinearLayoutManager(requireContext())
 
         reservationViewModel = ViewModelProvider(this)[ReservationViewModel::class.java]
+
+        var tempListOfReservations: MutableList<Reservation> = mutableListOf()
+        tempListOfReservations.add(Reservation(1, 1, "test1", "test detail", "12/12/12"))
+        tempListOfReservations.add(Reservation(2, 1, "test2", "test detail", "12/12/12"))
+        tempListOfReservations.add(Reservation(3, 1, "test3", "test detail", "12/12/12"))
+        tempListOfReservations.add(Reservation(4, 1, "test4", "test detail", "12/12/12"))
+        tempListOfReservations.add(Reservation(5, 1, "test5", "test detail", "12/12/12"))
+
         reservationViewModel.getAllData.observe(viewLifecycleOwner) {
-            reservations -> reservationAdapter.setData(reservations)
+            reservations -> reservationAdapter.setData(tempListOfReservations)
         }
 
         return binding.root
