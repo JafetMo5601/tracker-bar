23670385f192977eaed3d5f22d7ffeb16bca351b
JafetMo5601 <jafet.moraugalde15@gmail.com>
2022-04-10 22:47:22 -0600

    no message

--- app/src/main/java/com/finalproject/model/SupportMessage.kt
+++ app/src/main/java/com/finalproject/model/SupportMessage.kt
@@ -1,4 +1,14 @@
 package com.finalproject.model
 
-class SupportMessage {
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class SupportMessage(
+    var id: Int,
+    var user: String,
+    var message: String
+): Parcelable {
+    constructor():
+            this(0, "", "")
 }
