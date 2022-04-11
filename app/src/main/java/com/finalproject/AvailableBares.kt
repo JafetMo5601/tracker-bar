c8292c7b61cb5baab71b2203ac84acac9015d855
JafetMo5601 <jafet.moraugalde15@gmail.com>
2022-04-10 22:43:27 -0600

    Add firestore dependency to project Create bares page

--- app/src/main/java/com/finalproject/AvailableBares.kt
+++ app/src/main/java/com/finalproject/AvailableBares.kt
@@ -1,7 +1,7 @@
 package com.finalproject
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class AvailableBares : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
